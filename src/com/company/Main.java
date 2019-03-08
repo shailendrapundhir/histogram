cd15a92cc260dbaf5798238aa93e8aec43cfa60f
Shailendra Pundhir <shailendra.pundhir@propalmsnetwork.com>
2019-03-09 01:06:42 +0530

    Move file path to std input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,34 @@ package com.company;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File(args[0]);
-        ArrayList<String> values = new ArrayList<>();
+        BufferedReader bufferedReaderStdInput = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter input values file path:");
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String path = bufferedReaderStdInput.readLine();
+
+            ArrayList<String> values = new ArrayList<>();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
             String line;
-            while (((line = bufferedReader.readLine()) !=null)){
+            while (((line = bufferedReader.readLine()) != null)) {
                 values.add(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        for (String s : values) {
-            int sum = 0;
-            for (char ch : s.toCharArray()) {
-                sum += (int) ch;
+            for (String s : values) {
+                int sum = 0;
+                for (char ch : s.toCharArray()) {
+                    sum += (int) ch;
+                }
+
+                System.out.println("Ascii sum for \"" + s + "\" : " + sum);
             }
 
-            System.out.println("Ascii sum for \"" + s + "\" : " + sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Invalid inputs");
         }
     }
 }
